930a090448ef8b216e55e93784e7a81b7ad92170
Helayxa <axel.latour3@gmail.com>
2020-03-24 18:59:34 +0100

    Adding webhook connection to check if Bot can communicate with server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
 const Telegraf = require('telegraf');
-const express = require('express')();
+const express = require('express');
+const axios = require('axios');
 
 const bot = new Telegraf("1132298501:AAHW-k5TMwYISexLi3DyN0YTHBzDwxd3oW8");
+const app = express();
+
+app.use(bot.webhookCallback('/checkAnswer'));
+bot.telegram.setWebhook('http://adi3000.com:33333/checkAnswer');
 
 bot.on('text', ctx => {
   return ctx.reply(ctx.message.text);
 });
 
-express.listen(3000, () => {
+const generateQuestionsUrl = (req) => {
+  return `https://opentdb.com/api.php?amount=10`;
+};
+
+app.post('/checkAnswer', (req, res) => {
+  console.log(req.body);
+});
+
+app.get('/questions', (req, res) => {
+  axios({
+    method: 'get',
+    url: generateQuestionsUrl(req)
+  }).then(({ data: { results } }) => {
+    res.send(results);
+    console.log(results);
+  });
+});
+
+app.listen(33333, () => {
   console.log('Quiztiti is running !');
 });
