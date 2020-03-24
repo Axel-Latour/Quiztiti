f2890c88f158ed4e3448a280d1c14f094921f3b4
Helayxa <axel.latour3@gmail.com>
2020-03-24 21:16:15 +0100

    Trying polling for first version

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,33 @@ const axios = require('axios');
 const bot = new Telegraf("1132298501:AAHW-k5TMwYISexLi3DyN0YTHBzDwxd3oW8");
 const app = express();
 
-app.use(bot.webhookCallback('/checkAnswer'));
-bot.telegram.setWebhook('http://adi3000.com:33333/checkAnswer');
+// app.use(bot.webhookCallback('/checkAnswer'));
+// bot.telegram.setWebhook('http://adi3000.com:33333/checkAnswer');
 
 bot.on('text', ctx => {
   return ctx.reply(ctx.message.text);
 });
-
-const generateQuestionsUrl = (req) => {
-  return `https://opentdb.com/api.php?amount=10`;
-};
 
 app.post('/checkAnswer', (req, res) => {
   console.log(req.body);
 });
 
 app.get('/questions', (req, res) => {
+  const generateQuestionsUrl = (req) => {
+    return `https://opentdb.com/api.php?amount=10`;
+  };
+
   axios({
     method: 'get',
     url: generateQuestionsUrl(req)
   }).then(({ data: { results } }) => {
     res.send(results);
     console.log(results);
-  });
+  }, error => console.log(error));
 });
 
 app.listen(33333, () => {
   console.log('Quiztiti is running !');
 });
+
+bot.startPolling();
