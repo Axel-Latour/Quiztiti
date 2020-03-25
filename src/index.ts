6d84892872799f50aedab2411e523c8c97e80774
Helayxa <axel.latour3@gmail.com>
2020-03-25 10:16:14 +0100

    Testing bot start without express

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,30 @@
 const Telegraf = require('telegraf');
-const express = require('express');
-const axios = require('axios');
+// const express = require('express');
+// const axios = require('axios');
 
 const bot = new Telegraf("1132298501:AAHW-k5TMwYISexLi3DyN0YTHBzDwxd3oW8");
-const app = express();
-
-// app.use(bot.webhookCallback('/checkAnswer'));
-// bot.telegram.setWebhook('http://adi3000.com:33333/checkAnswer');
+// const app = express();
 
 bot.on('text', ctx => {
   return ctx.reply(ctx.message.text);
 });
-
-app.post('/message', (req, res) => {
-  console.log(req.body);
-});
-
-app.get('/questions', (req, res) => {
-  const generateQuestionsUrl = (req) => {
-    return `https://opentdb.com/api.php?amount=10`;
-  };
-
-  axios({
-    method: 'get',
-    url: generateQuestionsUrl(req)
-  }).then(({ data: { results } }) => {
-    res.send(results);
-    console.log(results);
-  }, error => console.log(error));
-});
-
-app.listen(33333, () => {
-  console.log('Quiztiti is running !');
-});
+//
+// app.get('/questions', (req, res) => {
+//   const generateQuestionsUrl = (req) => {
+//     return `https://opentdb.com/api.php?amount=10`;
+//   };
+//
+//   axios({
+//     method: 'get',
+//     url: generateQuestionsUrl(req)
+//   }).then(({ data: { results } }) => {
+//     res.send(results);
+//     console.log(results);
+//   }, error => console.log(error));
+// });
+//
+// app.listen(33333, () => {
+//   console.log('Quiztiti is running !');
+// });
 
 bot.startPolling();
