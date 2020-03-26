bf8a638935eaba65d6362b7269894c283d83ba86
Helayxa <axel.latour3@gmail.com>
2020-03-27 00:09:32 +0100

    Trying different commands for group chat

--- src/BotInitializer.ts
+++ src/BotInitializer.ts
@@ -2,13 +2,13 @@ import { ContextMessageUpdate } from 'telegraf';
 import { handleMessage, resetQuiz, sendRoundChoiceMessage, stopQuiz } from './index';
 
 export const initializeBot = (bot) => {
-  bot.command('start', (ctx) => {
+  bot.command('/start@helayxa_quiztiti_bot', (ctx) => {
     console.log('start command has been triggered');
     resetQuiz();
     sendRoundChoiceMessage(ctx, '');
   });
 
-  bot.command('stop', (ctx: ContextMessageUpdate) => {
+  bot.command('stop@helayxa_quiztiti_bot', (ctx: ContextMessageUpdate) => {
     console.log('stop command has been triggered');
     stopQuiz(ctx);
   });
